709619970d55dca77859f0d159026f40d7d353ac
juliuscanute <juliuscanute@gmail.com>
2023-10-29 15:50:30 +1100

    [N/a] Implement print functionality

--- lib/ui/revise/score_card.dart
+++ lib/ui/revise/score_card.dart
@@ -3,6 +3,9 @@ import 'package:intl/intl.dart';
 import 'package:learning_assistant/data/result.dart';
 import 'package:learning_assistant/data/result_repository.dart';
 import 'package:learning_assistant/di/service_locator.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pdf;
+import 'package:printing/printing.dart';
 
 class ScoreCardListScreen extends StatefulWidget {
   final resultRepository = ServiceLocator.instance.get<ResultRepository>();
@@ -17,6 +20,57 @@ class ScoreCardListScreen extends StatefulWidget {
 }
 
 class _ScoreCardListScreenState extends State<ScoreCardListScreen> {
+  final ValueNotifier<List<ResultGroup>> items =
+      ValueNotifier<List<ResultGroup>>([]);
+
+  void generatePdf(List<ResultGroup> scores) {
+    final pdf.Document doc = pdf.Document();
+    doc.addPage(
+      pdf.Page(
+        build: (pdf.Context context) {
+          return pdf.Table(children: [
+            pdf.TableRow(children: [
+              pdf.Container(
+                decoration: pdf.BoxDecoration(border: pdf.Border.all()),
+                child: pdf.Text('Date',
+                    style: pdf.TextStyle(
+                        fontSize: 6, fontWeight: pdf.FontWeight.bold)),
+              ),
+              pdf.Container(
+                decoration: pdf.BoxDecoration(border: pdf.Border.all()),
+                child: pdf.Text('Result',
+                    style: pdf.TextStyle(
+                        fontSize: 6, fontWeight: pdf.FontWeight.bold)),
+              ),
+            ]),
+            for (var i = 0; i < scores.length; i++)
+              pdf.TableRow(children: [
+                pdf.Container(
+                  decoration: pdf.BoxDecoration(border: pdf.Border.all()),
+                  child: pdf.Text(
+                      DateFormat('MMM d, y - HH:mm:ss').format(scores[i].date),
+                      style: const pdf.TextStyle(
+                          fontSize:
+                              6)), // Assuming reportedItems[i][0] contains the date
+                ),
+                pdf.Container(
+                  decoration: pdf.BoxDecoration(border: pdf.Border.all()),
+                  child: pdf.Text(
+                      "${scores[i].correct}/${scores[i].correct + scores[i].wrong + scores[i].missed}",
+                      style: const pdf.TextStyle(
+                          fontSize:
+                              6)), // Assuming reportedItems[i][1] contains the result
+                ),
+              ])
+          ]);
+        },
+      ),
+    );
+    Printing.layoutPdf(
+      onLayout: (PdfPageFormat format) async => doc.save(),
+    );
+  }
+
   @override
   void initState() {
     super.initState();
@@ -25,35 +79,49 @@ class _ScoreCardListScreenState extends State<ScoreCardListScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: StreamBuilder<List<ResultGroup>>(
-        stream: widget.resultRepository.events,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.data!.isEmpty) {
-            return const Center(child: Text('No results found.'));
-          } else {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (BuildContext context, int index) {
-                final group = snapshot.data![index];
-                return ScoreCardWidget(
-                  date: group.date,
-                  correct: group.correct,
-                  missed: group.missed,
-                  wrong: group.wrong,
-                );
-              },
+    return StreamBuilder<List<ResultGroup>>(
+      stream: widget.resultRepository.events,
+      builder: (context, snapshot) {
+        // Update the ValueNotifier value here
+        if (snapshot.hasData) {
+          items.value = snapshot.data!;
+        }
+        return ValueListenableBuilder(
+          valueListenable: items,
+          builder: (context, value, child) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text(widget.title),
+                actions: <Widget>[
+                  if (value.isNotEmpty)
+                    IconButton(
+                      icon: const Icon(Icons.print),
+                      onPressed: () {
+                        generatePdf(value);
+                      },
+                    ),
+                ],
+              ),
+              body: !snapshot.hasData
+                  ? const Center(child: CircularProgressIndicator())
+                  : snapshot.data!.isEmpty
+                      ? const Center(child: Text('No results found.'))
+                      : ListView.builder(
+                          itemCount: value.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final group = value[index];
+                            return ScoreCardWidget(
+                              date: group.date,
+                              correct: group.correct,
+                              missed: group.missed,
+                              wrong: group.wrong,
+                            );
+                          },
+                        ),
             );
-          }
-        },
-      ),
+          },
+        );
+      },
     );
   }
 }
